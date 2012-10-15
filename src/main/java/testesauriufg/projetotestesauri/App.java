b1046676be1e892f261d739752196fd3e1280a17
bhorist <borisvampire@gmail.com>
2012-10-15 04:24:07 -0300

    add novo projeto

---
+++ src/main/java/testesauriufg/projetotestesauri/App.java
@@ -0,0 +1,13 @@
+package testesauriufg.projetotestesauri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World de projeto Maven!" );
+    }
+}
